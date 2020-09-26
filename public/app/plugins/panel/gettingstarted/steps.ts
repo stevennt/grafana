6562122312da1a863f5bd80054e30adc087f7429
Steven <steven.n.t@outlook.com>
2020-09-27 01:06:06 +0700

    Docker build ok. Run ok not crash.

--- public/app/plugins/panel/gettingstarted/steps.ts
+++ public/app/plugins/panel/gettingstarted/steps.ts
@@ -17,7 +17,7 @@ const step2Key = `${keyPrefix}${step2TutorialTitle
 
 export const getSteps = (): SetupStep[] => [
   {
-    heading: 'Welcome to Grafana',
+    heading: 'ABN Data',
     subheading: 'The steps below will guide you to quickly finish setting up your Grafana installation.',
     title: 'Basic',
     info: 'The steps below will guide you to quickly finish setting up your Grafana installation.',
